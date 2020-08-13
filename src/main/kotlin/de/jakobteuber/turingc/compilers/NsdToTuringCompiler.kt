ee65d77bfb3311f8c34dd21c8a2a6cee565e1634
jteuber <jakob.teuber@gmx.de>
2020-08-13 16:18:48 +0200

    Nicer turin code

--- src/main/kotlin/de/jakobteuber/turingc/compilers/NsdToTuringCompiler.kt
+++ src/main/kotlin/de/jakobteuber/turingc/compilers/NsdToTuringCompiler.kt
@@ -12,7 +12,7 @@ import java.nio.file.Path
 
 
 class NsdToTuringCompiler(private val labels: Map<String, Int>) : NsdBaseListener() {
-    private val code = StringBuilder()
+    private val code = CodeBuilder(commentSign = "# ")
     private var currentState = 0
     private val variables = mutableMapOf<String, Int>()
 
@@ -32,200 +32,306 @@ class NsdToTuringCompiler(private val labels: Map<String, Int>) : NsdBaseListene
         return labels[label] ?: throw CompilerException(token, "label is undefined")
     }
 
-    private fun addCode(string: String) {
-        code.append(string)
-    }
-
     private fun moveToVar(varName: String, token: Token) {
         val varIndex = getVarIndex(varName, token)
-        addCode(
-            """
-            # Gehe zum Anfang
-            ((1 1 L +0)
-             (0 0 L +0)
-             (B B L +0)
-             (* * R +1))
-            # Gehe zur Variable $varName (index: $varIndex)
-        """.trimIndent()
-        )
-        repeat(varIndex) {
-            addCode(
+        code.comment("Gehe zur Variablen $varName (index: $varIndex)") {
+            comment("Gehe zum Anfang des Bandes") {
+                code(
+                    """
+                    (
+                    (1 1 L +0)
+                    (0 0 L +0)
+                    (B B L +0)
+                    (* * R +1)
+                    )
                 """
-                ((1 1 R +0)
-                 (0 0 R +0)
-                 (B B R +1))
-            """.trimIndent()
-            )
+                )
+            }
+            comment("Zum Index $varIndex überspringe die Variable 0 bis ${varIndex - 1}") {
+                repeat(varIndex) {
+                    code(
+                        """
+                        (
+                        (1 1 R +0)
+                        (0 0 R +0)
+                        (B B R +1)
+                        )
+                    """
+                    )
+                }
+            }
+            comment("Gehe zur letzten Stelle der Variablen") {
+                code(
+                    """
+                    (
+                    (1 1 R +0)
+                    (0 0 R +0)
+                    (B B L +1)
+                    )
+                """
+                )
+            }
         }
-        addCode(
-            """
-            # Gehe zur letzetn Stelle
-            ((1 1 R +0)
-             (0 0 R +0)
-             (B B L +1))
-        """.trimIndent()
-        )
         currentState += varIndex + 2
     }
 
-    private fun addDigit(varName: String, token: Token) {
-        moveToVar(varName, token)
-        addCode(
-            """
-            # Gehe an den Anfang der Variable
-            ((1 1 L +0)
-             (0 0 L +0)
-             (B B R +1)
-             (* * R +1))
-            # Füge führende Null hinzu
-            ((1 0 R +1)
-             (0 0 R +4)) # Keine Arbeit erforderlich
-            # Schieben
-            ((1 1 R +0)
-             (0 1 R +1)
-             (B 1 R +2))
-            ((1 0 R +0)
-             (0 0 R -1)
-             (B 0 R +2))
-            ((1 B R -1)
-             (0 B R -2)
-             (B B R +1))
-        """.trimIndent()
-        )
-        currentState += 5
-        moveToVar(varName, token)
-    }
+    private fun addDigit(varName: String, token: Token) =
+        code.comment("Füge eine führende Null bei der Variable $varName hinzu") {
+            moveToVar(varName, token)
+            comment("Gehe an den Anfang der Variable") {
+                code(
+                    """
+                    (
+                    (1 1 L +0)
+                    (0 0 L +0)
+                    (B B R +1)
+                    (* * R +1)
+                    )
+                """
+                )
+            }
+            comment("Füge führende Null hinzu") {
+                code(
+                    """
+                    (
+                    (1 0 R +1)
+                    (0 0 R +4) # Keine Arbeit erforderlich
+                    )
+                """
+                )
+            }
+            comment("Schiebe alle weiteren Stellen und Variable um 1 nach rechts") {
+                code(
+                    """
+                    (
+                    (1 1 R +0)
+                    (0 1 R +1)
+                    (B 1 R +2)
+                    )
+                    (
+                    (1 0 R +0)
+                    (0 0 R -1)
+                    (B 0 R +2)
+                    )
+                    (
+                    (1 B R -1)
+                    (0 B R -2)
+                    (B B R +1)
+                    )
+                """
+                )
+            }
+            currentState += 5
+            moveToVar(varName, token)
+        }
 
     override fun enterParameter(ctx: NsdParser.ParameterContext) {
         registerVar(ctx.name.text, ctx.name)
     }
 
-    override fun enterIncrement(ctx: NsdParser.IncrementContext) {
-        addDigit(ctx.varName.text, ctx.varName)
-        addCode(
-            """
-            # ==== ${ctx.humanizedText} ====
-            ((1 1 L +0)
-             (0 1 L +1))
-        """.trimIndent()
-        )
-        currentState++
-    }
+    override fun enterIncrement(ctx: NsdParser.IncrementContext) =
+        code.comment(ctx.humanizedText) {
+            addDigit(ctx.varName.text, ctx.varName)
+            code(
+                """
+            (
+            (1 1 L +0)
+            (0 1 L +1)
+            )
+        """
+            )
+            currentState++
+        }
 
-    override fun enterDecrement(ctx: NsdParser.DecrementContext) {
-        moveToVar(ctx.varName.text, ctx.varName)
-        addCode(
-            """
-            # ==== ${ctx.humanizedText} ====
-            ((1 0 L +1)
-             (0 0 L +0)
-             (B B L +1)
-             (* * L +1))
-        """.trimIndent()
-        )
-        currentState++
-    }
+    override fun enterDecrement(ctx: NsdParser.DecrementContext) =
+        code.comment(ctx.humanizedText) {
+            moveToVar(ctx.varName.text, ctx.varName)
+            code(
+                """
+            (
+            (1 0 L +1)
+            (0 0 L +0)
+            (B B L +1)
+            (* * R +1)
+            )
+        """
+            )
+            currentState++
+        }
 
     override fun enterLabel(ctx: NsdParser.LabelContext) {
         // Ignore
         // Labels are handled in the LabelVisitor on first pass.
+        code.comment("Label Registered: ${ctx.label.text}")
     }
 
-    override fun enterGoto(ctx: NsdParser.GotoContext) {
-        moveToVar(ctx.varName.text, ctx.varName)
-        currentState += 2
-        val newState = getLabelIndex(ctx.label.text, ctx.label)
-        val changeState = newState - currentState
-        addCode(
-            """
-            # ==== ${ctx.humanizedText} ====
-            # Sprung zum Label ${ctx.label.text} wenn ${ctx.varName.text} == 0
-            ((0 0 L +0)
-             (1 1 L +3)
-             (B B R +1)
-             (* * R +1))
-            ((0 0 R 0)
-             (1 1 R 0)
-             (B B L $changeState))
-        """.trimIndent()
-        )
-    }
+    override fun enterGoto(ctx: NsdParser.GotoContext) =
+        code.comment(ctx.humanizedText) {
+            moveToVar(ctx.varName.text, ctx.varName)
+            currentState += 2
+            val newState = getLabelIndex(ctx.label.text, ctx.label)
+            val changeState = newState - currentState
+            code.comment("Sprung zum Label ${ctx.label.text} wenn ${ctx.varName.text} == 0") {
+                code(
+                    """
+                        (
+                        (0 0 L +0)
+                        (1 1 L +2)
+                        (B B R +1)
+                        (* * R +1)
+                        )
+                        (
+                        (0 0 R +0)
+                        (1 1 R +0)
+                        (B B L $changeState)
+                        )
+                """
+                )
+            }
+        }
 
-    override fun enterInit(ctx: NsdParser.InitContext) {
-        registerVar(ctx.varName.text, ctx.varName)
-        addCode(
-            """
-            # ==== ${ctx.humanizedText} ====
-            # Gehe ganz ans Ende
-            ((0 0 R +0)
-             (1 1 R +0)
-             (B B R +1))
-            ((0 0 R -1)
-             (1 1 R -1)
-             (B B R +1)
-            ((B B L +1))
-            # Fuege Variable ${ctx.varName.text} hinzu
-            ((B 0 L +1))
-        """.trimIndent()
-        )
-        currentState += 4
-    }
+    override fun enterInit(ctx: NsdParser.InitContext) =
+        code.comment(ctx.humanizedText) {
+            registerVar(ctx.varName.text, ctx.varName)
+            comment("Gehe ganz ans Ende") {
+                code(
+                    """
+                    (
+                    (0 0 R +0)
+                    (1 1 R +0)
+                    (B B R +1)
+                    )
+                    (
+                    (0 0 R -1)
+                    (1 1 R -1)
+                    (B B R +1)
+                    )
+                    (
+                    (B B L +1)
+                    )
+                """
+                )
+            }
+            comment("Fuege Variable ${ctx.varName.text} hinzu") {
+                code(
+                    """
+                    (
+                    (B 0 L +1)
+                    )
+                """
+                )
+            }
+            currentState += 4
+        }
 
-    override fun enterReturn(ctx: NsdParser.ReturnContext) {
-        moveToVar(ctx.varName.text, ctx.varName)
-        addCode(
-            """
-            # ==== ${ctx.humanizedText} ====
-            ((0 0 R +1)
-             (1 1 R +1)
-            # Alles hinter Variable ${ctx.varName.text} loeschen
-            ((0 B R +0)
-             (1 B R +0)
-             (B B R +1))
-            ((0 B R -1)
-             (1 B R -1)
-             (B B R +1))
-            # Zurueck zur letzten Variablen
-            ((B B L +0)
-             (0 0 L +1)
-             (1 1 L +1))
-            # Gehe an den Anfang der Variablen
-            ((0 0 L +0)
-             (1 1 L +0)
-             (B B L +1)
-             (* * R H))
-            # Alles vor Variable ${ctx.varName} loeschen
-            ((0 B L +0)
-             (1 B L +0)
-             (B B L +0)
-             (* * R +1))
-            # Variable ${ctx.varName} an den Anfang schieben
-            ((B B R +0)
-             (1 1 R +1)
-             (0 0 R +1))
-            ((1 1 R +0)
-             (0 0 R +0)
-             (B B L +1))
-            ((1 B L +1) # Zuletzt wurde ein B gelesen
-             (0 B L +2))
-            ((1 1 L +0) # Zuletzt wurde eine 1 gelesen
-             (0 1 L +1)
-             (B 1 R -3)
-             (* * R +2))
-            ((1 0 L -1) # Zuletzt wurde eine 0 gelesen
-             (0 0 L +0)
-             (B 0 R -4)
-             (* * R +2))
-            ((1 1 R +0)
-             (0 1 R +1)
-             (B 1 R H))
-            ((0 0 R +0)
-             (1 0 R -1)
-             (B 0 R H))
-        """.trimIndent()
-        )
-        currentState += 13
-    }
+    override fun enterReturn(ctx: NsdParser.ReturnContext) =
+        code.comment(ctx.humanizedText) {
+            moveToVar(ctx.varName.text, ctx.varName)
+            comment("Gehe hinter die Variable") {
+                code(
+                    """
+                    (
+                    (0 0 R +1)
+                    (1 1 R +1)
+                    )
+                """
+                )
+            }
+            comment("Alles hinter Variable ${ctx.varName.text} loeschen") {
+                code(
+                    """
+                    (
+                    (0 B R +0)
+                    (1 B R +0)
+                    (B B R +1)
+                    )
+                    (
+                    (0 B R -1)
+                    (1 B R -1)
+                    (B B R +1)
+                    )
+                """
+                )
+            }
+            comment("Zurueck zur letzten Variablen") {
+                code(
+                    """
+                    (
+                    (B B L +0)
+                    (0 0 L +1)
+                    (1 1 L +1)
+                    )
+                """
+                )
+            }
+            comment("Gehe an den Anfang der Variablen") {
+                code(
+                    """
+                    (
+                    (0 0 L +0)
+                    (1 1 L +0)
+                    (B B L +1)
+                    (* * R H)
+                    )
+                """
+                )
+            }
+            comment("Alles vor Variable ${ctx.varName} loeschen") {
+                code(
+                    """
+                    (
+                    (0 B L +0)
+                    (1 B L +0)
+                    (B B L +0)
+                    (* * R +1)
+                    )
+                """
+                )
+            }
+            comment("Variable ${ctx.varName} an den Anfang schieben") {
+                code(
+                    """
+                    (
+                    (B B R +0)
+                    (1 1 R +1)
+                    (0 0 R +1)
+                    )
+                    (
+                    (1 1 R +0)
+                    (0 0 R +0)
+                    (B B L +1)
+                    )
+                    (
+                    (1 B L +1) # Zuletzt wurde ein B gelesen
+                    (0 B L +2)
+                    )
+                    (
+                    (1 1 L +0) # Zuletzt wurde eine 1 gelesen
+                    (0 1 L +1)
+                    (B 1 R -3)
+                    (* * R +2)
+                    )
+                    (
+                    (1 0 L -1) # Zuletzt wurde eine 0 gelesen
+                    (0 0 L +0)
+                    (B 0 R -4)
+                    (* * R +2)
+                    )
+                    (
+                    (1 1 R +0)
+                    (0 1 R +1)
+                    (B 1 R H)
+                    )
+                    (
+                    (0 0 R +0)
+                    (1 0 R -1)
+                    (B 0 R H)
+                    )
+                """
+                )
+            }
+            currentState += 13
+        }
 
     companion object {
         fun compile(code: String) = compile(CharStreams.fromString(code))
